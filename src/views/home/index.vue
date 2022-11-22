81abb660a8903ae3413c702e7b026c39cf0ac172
huangcheng <huangcheng@xmov.ai>
2022-11-22 20:30:16 +0800

    feat: 添加备案号

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="bg-black h-100vh w-full flex justify-center items-center">
+  <div class="relative h-100vh w-full flex justify-center items-center bg-black">
     <div class="text-center text-white">
       <img class="h-300px mb-40px mx-auto" src="@/assets/logo.svg" alt="">
-      <div class="mt-20px">
-        <router-link to="about" class="text-blue-600">打开About</router-link>
-      </div>
+    </div>
+    <div class="absolute bottom-0 left-0 w-full pb-16px text-center text-blue-500">
+      <a href="https://beian.miit.gov.cn/" target="_blank">赣ICP备19012553号-1</a>
     </div>
   </div>
 </template>
